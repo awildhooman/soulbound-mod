a4df7f42080e92ab25a4e39e68fad86da3bf8c5a
Souvik Basak <souvik.b416@gmail.com>
2024-07-03 14:17:55 -0400

    remove unused imports

--- src/main/java/soulbound/mixin/SoulboundPlayerInventoryMixin.java
+++ src/main/java/soulbound/mixin/SoulboundPlayerInventoryMixin.java
@@ -3,20 +3,13 @@ package soulbound.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.collection.DefaultedList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.List;
 
 @Mixin(PlayerInventory.class)
 public class SoulboundPlayerInventoryMixin {
